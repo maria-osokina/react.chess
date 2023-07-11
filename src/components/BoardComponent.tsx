1b05507001152941a779c55c4ee0abb047b9bc3a
Maria Osokina <osokina.m@medindex.ru>
2023-07-11 17:35:45 +0300

    Движение фигур. Ферзь, слон, ладья.

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,19 +1,52 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { Board } from '../models/Board';
 import CellComponent from './CellComponent';
+import { Cell } from '../models/Cell';
 
 interface BoardProps {
     board: Board;
     setBoard: (board: Board) => void;
 }
 
-const BoardComponent: FC<BoardProps> = ({ board, setBoard}) => {
+const BoardComponent: FC<BoardProps> = ({ board, setBoard }) => {
+    const [selectedCell, setSelectedCell] = useState<Cell | null>(null);
+
+    function click(cell: Cell) {
+        if (selectedCell && selectedCell !== cell && selectedCell.figure?.canMove(cell)) {
+            selectedCell.moveFigure(cell);
+            setSelectedCell(null);
+        }
+        else {
+            setSelectedCell(cell);
+        }
+    }
+
+    function highlightCells() {
+        board.highlightCells(selectedCell);
+        updateBoard();
+    }
+
+    function updateBoard() {
+        const newBoard = board.getCopyBoard();
+        setBoard(newBoard);
+    }
+
+    useEffect(() => {
+        highlightCells();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [selectedCell]);
+
     return (
         <div className="board">
             {board.cells.map((row, index) =>
                 <React.Fragment key={index}>
                     {row.map((cell) =>
-                        <CellComponent cell={cell} key={cell.id} />
+                        <CellComponent
+                            cell={cell}
+                            key={cell.id}
+                            selected={cell.x === selectedCell?.x && cell.y === selectedCell?.y}
+                            click={click}
+                        />
                     )}
                 </React.Fragment>
             )}
